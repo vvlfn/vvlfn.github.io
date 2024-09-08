1c164c3da178be905a19ba8e267e4b568bc46123
vvlfn <szkolapawel16@gmail.com>
2024-09-08 16:18:42 +0200

    add redirect

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+let thirdDec = new Date("December 3, 2023 00:00:00").getTime();
+
+let timer = setInterval(function () {
+	let now = new Date().getTime();
+	let distance = now - thirdDec;
+
+	// console.log(distance);
+	let days = Math.ceil(distance / 1000 / 60 / 60 / 24);
+	document.getElementById("timer").innerHTML = `${days} dni`;
+}, 1000);
